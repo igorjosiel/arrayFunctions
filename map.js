a7e2f743091e505e12dd774641cc08e4ae678830
Igor Josiel <igorjosielvitalino@gmail.com>
2023-03-03 10:33:10 -0300

    criando arquivo para praticar a função map.

---
+++ map.js
@@ -0,0 +1,18 @@
+//                                      map()
+// ========================================================================================
+
+const numbers = [1, 4, 5, 9, 14, 23];
+
+// ===================================== Dobrar valores ===================================
+
+const doubledNumbers = numbers.map((number) => {
+  return number * 2;
+});
+
+// ================================= Fahrenheit para Celcius ==============================
+
+const fahrenheit = [0, 32, 45, 50, 75, 80, 120];
+
+const celcius = fahrenheit.map((elem) => Math.round((elem - 32) * 5/9));
+
+console.log(celcius);
