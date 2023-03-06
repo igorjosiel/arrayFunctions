063ac0944314085da47799246006695d3813b50d
Igor Josiel <igorjosielvitalino@gmail.com>
2023-03-06 17:33:35 -0300

    fix: ajuste.

--- API.js
+++ API.js
@@ -9,20 +9,20 @@ async function getPeople() {
 getPeople().then(data => {
   const people = data.results;
 
-  console.log(people.filter(p => p.gender === "female"));
+  console.log(people.filter(person => person.gender === "female"));
 });
 
 // Trabalhando com dados
 
 getPeople().then(data => {
-  const result = data.results.filter(p => p.dob.age >= 30);
+  const result = data.results.filter(person => person.dob.age >= 30);
   const people = [];
 
   for (let p of result) {
     people.push({
-      "Nome": `${p.name.first} ${p.name.last}`,
-      "Sexo": `${p.gender}`,
-      "Idade": `${p.dob.age}`,
+      "Nome": `${person.name.first} ${person.name.last}`,
+      "Sexo": `${person.gender}`,
+      "Idade": `${person.dob.age}`,
     });
   }
 
